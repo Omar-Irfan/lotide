e3df90a006c7738023f2c3b93ba0df42ede44872
Omar Irfan <omar.irfan18@gmail.com>
2021-03-29 19:54:13 +0000

    Improved assertEqual Function

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅️✅️✅️ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🚫️🚫️🚫️ Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🚫️🚫️🚫️ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
