e0664dca2e91dee159e6674495ef903ec5fe2ef7
Omar Irfan <omar.irfan18@gmail.com>
2021-03-30 20:03:35 +0000

    created eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫️🚫️🚫️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    } return true;
+  } else {
+    return false;
+  }
+
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
