a7018d2b8c826d90001530f2d8956071bdc99282
Omar Irfan <omar.irfan18@gmail.com>
2021-04-05 17:04:47 +0000

    refactored middle

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    } return true;
-  } else {
-    return false;
-  }
-
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected) === true) {
-    console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚫️🚫️🚫️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const middle = function(array) {
   let midIndex = (array.length - 1) / 2;
   let midElements = [];
@@ -36,9 +15,4 @@ const middle = function(array) {
   }
   return midElements;
 };
-
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-assertArraysEqual(middle([1]), []);
+module.exports = middle
